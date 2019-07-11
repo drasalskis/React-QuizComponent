c7316ce7b36a3dafc8116eddaef1dd6c2c72efc5
Domantas Rasalskis <drasalskis@kaunoenergija.lt>
2019-07-11 22:21:38 +0300

    module4

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 import QuizQuestion from './QuizQuestion.js'
+import QuizEnd from './QuizEnd.js'
 
 let quizData = require('./quiz_data.json')
 
@@ -11,7 +12,13 @@ class Quiz extends Component {
     }
 
     render() {
-        return <div><QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/></div>
+        const isQuizEnd = (this.state.quiz_position - 1) === quizData.quiz_questions.length        
+        return <div>
+            {isQuizEnd ?
+                <QuizEnd/>:
+                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
+            }
+            </div>
     }
 }
 
